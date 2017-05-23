2c425b9c7e967304e639b206fd846b9074175cde
Edyta Wrobel <edytawrobel@Edytas-MacBook-Pro.local>
2017-05-23 18:10:52 +0100

    print vertical line & print horizontal line

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,10 +1,34 @@
 package com.example;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("asterisk" );
+
         System.out.println('*');
+
+        System.out.println("vertical line" );
+        verticalLine(8);
+
+        System.out.println("");
+        System.out.println("horizontal line" );
+        horizontalLine(3);
+    }
+
+    static void verticalLine(int n) {
+        if (n > 0) {
+            System.out.print('*');
+            verticalLine(n-1);
+        }
+    }
+
+    static void horizontalLine(int n) {
+        for (int i=0; i < n; i++) {
+            System.out.println('*');
+        }
     }
 
 
